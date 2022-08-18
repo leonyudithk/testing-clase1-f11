f0d5729fde8bdb4341ffa0a136ac392b81f6a2e0
yudithkleon <leonyudithk@gmail.com>
2022-08-18 10:25:42 -0500

    instalaciones

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -10,7 +10,6 @@ export const GifExpertApp = ({defaultCategories = []}) => {
             <h2>GifExpertApp</h2>
             <AddCategory setCategories={ setCategories } />
             <hr />
-
             <ol>
                 {
                     categories.map( category  => (
